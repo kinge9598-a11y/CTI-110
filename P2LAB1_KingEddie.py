ad7b983dc98ba6e07dada777f9aed5e59f4abe4a
kinge9598-a11y <kinge9598@student.faytechcc.edu>
2026-09-24 16:25:49 -0400

    Add files via upload

---
+++ P2LAB1_KingEddie.py
@@ -0,0 +1,29 @@
+# Eddie King
+# September 23, 2026
+# P2LAB1 - Circle Calculations
+# This program calculates the diameter, circumference, and area of a circle using a radius entered by the user.
+
+#Import math module to use the constant, math.pi
+import math
+
+#Get radius from user
+radius = float(input("What is the radius of the circle? "))
+print()
+
+#calculate diameter
+diameter = 2 * radius
+
+#Display diameter with 1 decimal point
+print(f"The diameter of the circle is {diameter:.1f}\n")
+
+#Calculate circumference
+circumference = 2 * math.pi * radius
+
+#Display circumference with 2 decimal places
+print(f"The circumference of the circle is {circumference:.2f}\n")
+
+#calculate area
+area = math.pi * radius**2
+
+#Display area with 3 decimal places
+print(f"The area of the circle is {area:.3f}")
